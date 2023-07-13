1265108cb15923f47ffeea5e8664d3f18c866988
Ricardo Carriel <rick01.carriel@gmail.com>
2023-07-12 23:02:36 -0300

    Etapa 7
     Trocando Turnos

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ function Square(props){
 }
 
 class Board extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+    
     renderSquare(i){
         return <Square value={this.state.squares[i]}
         onClick={() => this.handleClick(i)}
@@ -18,11 +26,14 @@ class Board extends React.Component{
     }
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+        xIsNext: !this.state.xIsNext,
+        });
     }
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : "O");
         return(
             <div>
                 <div className="status">{status}</div>
